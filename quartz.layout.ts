5e4f57fa027ae7ed8f0a6e47ef1d9e4ad54dced5
kyriakos33 <kyriakos.s.afentoulis@gmail.com>
2024-01-18 01:38:36 +0200

    Quartz sync: Jan 18, 2024, 1:38 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -24,12 +24,13 @@ export const defaultContentPageLayout: PageLayout = {
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
+    // Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    // Component.Graph(),
+    Component.Search(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
