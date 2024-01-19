0e536b2777fb059a6140801106d5cb2a1643edde
Odhynn <galanody@gmail.com>
2024-01-19 10:13:16 +0100

    mobile layout

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -26,13 +26,13 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    // Component.DesktopOnly(Component.Explorer()),
-    Component.Explorer(),
+    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
     // Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
+    Component.MobileOnly(Component.Explorer()),
   ],
 }
 
